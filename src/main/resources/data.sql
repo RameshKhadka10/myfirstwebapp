44395b59908944587fabddb685eeb0127bc52e25
RameshKhadka <itskhadka.ramesh@gmail.com>
2023-01-31 11:32:13 +0545

    added mysql database

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into todo (ID, USERNAME,DESCRIPTION,TARGET_DATE,DONE)
-values(10001, 'ramesh', 'Get AWS Certified', CURRENT_DATE(), false);
-
-insert into todo (ID, USERNAME,DESCRIPTION,TARGET_DATE,DONE)
-values(10002, 'suresh', 'Get spring boot Certified', CURRENT_DATE(), false);
-
-insert into todo (ID, USERNAME,DESCRIPTION,TARGET_DATE,DONE)
-values(10003, 'ganesh', 'Get full stack Certified', CURRENT_DATE(), false);
